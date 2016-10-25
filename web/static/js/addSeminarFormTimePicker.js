9c894329c7efa782097dd95255cd46ed83a09f0d
13668 <ferenc.beutel@nordakademie.de>
2016-10-25 13:14:37 +0200

    Fix seminar for JQuery selector

--- web/static/js/addSeminarFormTimePicker.js
+++ web/static/js/addSeminarFormTimePicker.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     /** TODO: Language beim Datepicker auch in ausgelagerter JS-Datei anpassen (AddSeminar) **/
     var language = window.navigator.language;
-    var start_date_input = $('input[name="startDate"]');
+    var start_date_input = $('#startDate');
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     start_date_input.datepicker({
         language: language,
@@ -10,7 +10,7 @@ $(document).ready(function () {
         todayHighlight: true,
         autoclose: true
     });
-    var end_date_input = $('input[name="endDate"]');
+    var end_date_input = $('#endDate');
     end_date_input.datepicker({
         language: language,
         format: 'mm/dd/yyyy',

c938f3f568517239f81ec157fad2b4b9cf85df22
DerHennar <Hendrik.Peters@nordakademie.de>
2016-11-02 11:12:30 +0100

    implement automatic creation of seminars.

--- src/main/java/de/nordakademie/multiplechoice/action/InitializeApplicationAction.java
+++ src/main/java/de/nordakademie/multiplechoice/action/InitializeApplicationAction.java
@@ -2,10 +2,12 @@ package de.nordakademie.multiplechoice.action;
 
 
 import de.nordakademie.multiplechoice.model.Lecturer;
+import de.nordakademie.multiplechoice.model.Seminar;
 import de.nordakademie.multiplechoice.model.Student;
 
 import de.nordakademie.multiplechoice.service.LecturerService;
 
+import de.nordakademie.multiplechoice.service.SeminarService;
 import de.nordakademie.multiplechoice.service.StudentService;
 
 import lombok.Getter;
@@ -13,6 +15,8 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /**
@@ -27,11 +31,16 @@ public class InitializeApplicationAction extends BaseAction {
     @Autowired
     private LecturerService lecturerService;
 
+    @Autowired
+    private SeminarService seminarService;
+
     @Setter
     private Integer quantStudents ;
 
     @Setter
     private Integer quantLecturer ;
+
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 
     ArrayList<String> studentFirstNameList = new ArrayList<String>();
@@ -39,17 +48,23 @@ public class InitializeApplicationAction extends BaseAction {
     ArrayList<String> lecturerFirstNameList = new ArrayList<String>();
     ArrayList<String> lecturerLastNameList = new ArrayList<String>();
     ArrayList<String> userEmailList = new ArrayList<String>();
-
+    ArrayList<String> seminarNameList = new ArrayList<String>();
+    ArrayList<String> seminarDescriptionList = new ArrayList<String>();
+    ArrayList<String> lecturerList = new ArrayList<String>();
 
     int arrayposition;
+    int maxParticipants;
     String firstName;
     String lastName;
     String email;
+    String seminarName;
+    String seminarDescription;
 
     public String initialize(){
         fillNameList();
         createStudent(quantStudents);
         createLecturer(quantLecturer);
+        createSeminar(10);
         return SUCCESS;
     };
 
@@ -81,7 +96,7 @@ public class InitializeApplicationAction extends BaseAction {
         studentLastNameList.add("Schmipmann");
         studentLastNameList.add("Schniepel");
         studentLastNameList.add("Schwarso");
-        lecturerFirstNameList.add("Schololwol");
+        lecturerFirstNameList.add("Jürgen");
         lecturerFirstNameList.add("Anna");
         lecturerFirstNameList.add("Heinrich");
         lecturerFirstNameList.add("Max");
@@ -103,17 +118,24 @@ public class InitializeApplicationAction extends BaseAction {
         lecturerLastNameList.add("Schniepel");
         lecturerLastNameList.add("Schwarso");
         lecturerLastNameList.add("Schololwol");
-
+        seminarNameList.add("Seminar1");
+        seminarNameList.add("Seminar2");
+        seminarNameList.add("Seminar3");
+        seminarNameList.add("Seminar4");
+        seminarNameList.add("Seminar5");
+        seminarDescriptionList.add("SeminarBeschreibung1");
+        seminarDescriptionList.add("SeminarBeschreibung2");
+        seminarDescriptionList.add("SeminarBeschreibung3");
+        seminarDescriptionList.add("SeminarBeschreibung4");
+        seminarDescriptionList.add("SeminarBeschreibung5");
     }
 
     public void createStudent(int number) {
-
-
-        for (int i = 1; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
             Student student = new Student();
-            arrayposition = nextRandomNumber(studentFirstNameList.size()-1);
+            arrayposition = (nextRandomNumber(studentFirstNameList.size()-1)-1);
             firstName = studentFirstNameList.get(arrayposition);
-            arrayposition = nextRandomNumber(studentLastNameList.size()-1);
+            arrayposition = (nextRandomNumber(studentLastNameList.size()-1)-1);
             lastName = studentLastNameList.get(arrayposition);
             email = (firstName + "." + lastName + "@nordakademie.de");
             if (userEmailList.contains(email)){
@@ -131,12 +153,11 @@ public class InitializeApplicationAction extends BaseAction {
     };
 
     public void createLecturer(int number) {
-
-        for (int i = 1; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
             Lecturer lecturer = new Lecturer();
-            arrayposition = nextRandomNumber(lecturerFirstNameList.size()-1);
+            arrayposition = (nextRandomNumber(lecturerFirstNameList.size()-1)-1);
             firstName = lecturerFirstNameList.get(arrayposition);
-            arrayposition = nextRandomNumber(lecturerLastNameList.size()-1);
+            arrayposition = (nextRandomNumber(lecturerLastNameList.size()-1)-1);
             lastName = lecturerLastNameList.get(arrayposition);
             email = (firstName + "." + lastName + "@nordakademie.de");
             if (userEmailList.contains(email)){
@@ -150,7 +171,28 @@ public class InitializeApplicationAction extends BaseAction {
             lecturer.setPassword("Passwort1");
             lecturerService.save(lecturer);
             userEmailList.add(email);
+            lecturerList.add(email);
         }
     };
+
+    public void createSeminar(int number) {
+        for (int i = 1; i <= number; i++) {
+            Seminar seminar = new Seminar();
+            arrayposition = (nextRandomNumber(seminarNameList.size()-1)-1);
+            seminarName = seminarNameList.get(arrayposition);
+            arrayposition = (nextRandomNumber(seminarDescriptionList.size()-1)-1);
+            seminarDescription = seminarDescriptionList.get(arrayposition);
+            maxParticipants = (20 + nextRandomNumber(10));
+            arrayposition = (nextRandomNumber(lecturerList.size()-1)-1);
+            Lecturer lecturer = lecturerService.findByMail(lecturerList.get(arrayposition));
+            seminar.setLecturer(lecturer);
+            seminar.setName(seminarName);
+            seminar.setDescription(seminarDescription);
+            seminar.setBeginDate(LocalDate.parse("2017-01-01", dateFormatter));
+            seminar.setEndDate(LocalDate.parse("2017-02-17", dateFormatter));
+            seminar.setMaxParticipants(maxParticipants);
+            seminarService.createOrUpdate(seminar);
+        }
+    }
 
 }

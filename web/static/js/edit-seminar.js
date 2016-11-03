c638d89aa85b9f1e532a0967f5d92edbbf257350
Ferenc Beutel <ferenc.beutel@nordakademie.de>
2016-11-03 19:10:22 +0100

    Fix date calculations for disabling forms

--- web/static/js/edit-seminar.js
+++ web/static/js/edit-seminar.js
@@ -2,6 +2,6 @@
  * Created by Max on 02.11.2016
  */
 var startDate = new Date($('#startDate').val());
-if (startDate.getDate() <= new Date()) {
+if (startDate <= new Date()) {
     $(':input').prop('disabled', true);
 }
